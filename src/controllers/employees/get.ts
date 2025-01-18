d7fbec2a06d933ec8ee645c1cc29e10d0b309def
peeranat-dan <ppeeranat.d@gmail.com>
2025-01-18 16:34:26 +0700

    feat: add get by id route

--- src/controllers/employees/get.ts
+++ src/controllers/employees/get.ts
@@ -3,7 +3,8 @@ import type { EmployeeService } from "../../types/services/employee.js"
 import * as S from "effect/Schema"
 import { Hono } from "hono"
 import { describeRoute } from "hono-openapi"
-import { EmployeeWithRelationsSchema, Helpers } from "../../schema/index.js"
+import { resolver, validator } from "hono-openapi/effect"
+import { Branded, EmployeeWithRelationsSchema, Helpers } from "../../schema/index.js"
 
 const getManyResponseSchema = S.Array(EmployeeWithRelationsSchema.Schema.omit("deletedAt"))
 
@@ -18,8 +19,28 @@ const getManyDocs = describeRoute({
       description: "Get Employees",
     },
   },
-  tags: ["Employees"],
+  tags: ["Employee"],
 })
+
+const getByIdResponseSchema = EmployeeWithRelationsSchema.Schema.omit("deletedAt")
+
+const getByIdDocs = describeRoute({
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: resolver(getByIdResponseSchema),
+        },
+      },
+      description: "Get Employee by EmployeeId",
+    },
+  },
+  tags: ["Employee"],
+})
+
+const validateGetByIdRequest = validator("param", S.Struct({
+  employeeId: Branded.EmployeeIdFromString,
+}))
 
 export function setupEmployeeGetRoutes(employeeService: EmployeeService) {
   const app = new Hono()
@@ -27,6 +48,12 @@ export function setupEmployeeGetRoutes(employeeService: EmployeeService) {
   app.get("/", getManyDocs, async (c) => {
     const employees = await employeeService.findMany()
     return c.json(Helpers.fromObjectToSchema(getManyResponseSchema)(employees), 200)
+  })
+
+  app.get("/:employeeId", getByIdDocs, validateGetByIdRequest, async (c) => {
+    const employeeId = c.req.valid("param").employeeId
+    const employee = await employeeService.findOneById(employeeId)
+    return c.json(Helpers.fromObjectToSchema(getByIdResponseSchema)(employee), 200)
   })
 
   return app
