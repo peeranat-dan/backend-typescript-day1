45c11a1beb0ba144ea7c0bd0614fc1542f890c1a
peeranat-dan <ppeeranat.d@gmail.com>
2025-01-18 16:31:46 +0700

    feat: add create employee routes

--- src/controllers/employees/index.ts
+++ src/controllers/employees/index.ts
@@ -1,11 +1,13 @@
 import type { EmployeeService } from "../../types/services/employee.js"
 import { Hono } from "hono"
 import * as EmployeeGetRoutes from "./get.js"
+import * as EmployeePostRoutes from "./post.js"
 
 export function setupEmployeeRoutes(employeeService: EmployeeService) {
   const app = new Hono()
 
   app.route("/", EmployeeGetRoutes.setupEmployeeGetRoutes(employeeService))
+  app.route("/", EmployeePostRoutes.setupEmployeePostRoutes(employeeService))
 
   return app
 }
---
+++ src/controllers/employees/post.ts
@@ -0,0 +1,35 @@
+import type { EmployeeService } from "../../types/services/employee.js"
+import { Hono } from "hono"
+import { describeRoute } from "hono-openapi"
+import { validator } from "hono-openapi/effect"
+import { EmployeeSchema, Helpers } from "../../schema/index.js"
+
+const CreateEmployeeResponseSchema = EmployeeSchema.Schema
+
+const createEmployeeDocs = describeRoute({
+  responses: {
+    201: {
+      content: {
+        "application/json": {
+          schema: CreateEmployeeResponseSchema,
+        },
+      },
+      description: "Create Employee",
+    },
+  },
+  tags: ["Employees"],
+})
+
+const validateCreateEmployee = validator("json", EmployeeSchema.CreateSchema)
+
+export function setupEmployeePostRoutes(employeeService: EmployeeService) {
+  const app = new Hono()
+
+  app.post("/", createEmployeeDocs, validateCreateEmployee, async (c) => {
+    const body = c.req.valid("json")
+    const newEmployee = await employeeService.create(body)
+    return c.json(Helpers.fromObjectToSchema(CreateEmployeeResponseSchema)(newEmployee), 201)
+  })
+
+  return app
+}
