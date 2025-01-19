471d1d33fa3026fda4e378ef078269894f129447
peeranat-dan <ppeeranat.d@gmail.com>
2025-01-19 14:03:26 +0700

    refactor: change employeee repository implementation to use Effect

--- src/repositories/employees/index.ts
+++ src/repositories/employees/index.ts
@@ -1,12 +1,14 @@
 import type { PrismaClient } from "@prisma/client"
 
 import type * as Types from "../../types/repositories/employee.js"
+import { Context, Effect, Layer } from "effect"
+import PrismaClientContext from "../prisma.js"
 import * as Creates from "./creates.js"
 import * as Finds from "./finds.js"
 import * as Removes from "./removes.js"
 import * as Updates from "./updates.js"
 
-export default function initEmployeeRepository(prismaClient: PrismaClient): Types.EmployeeRepository {
+function initEmployeeRepository(prismaClient: PrismaClient): Types.EmployeeRepository {
   return {
     create: Creates.create(prismaClient),
     findById: Finds.findById(prismaClient),
@@ -18,4 +20,11 @@ export default function initEmployeeRepository(prismaClient: PrismaClient): Type
     update: Updates.update(prismaClient),
     updatePartial: Updates.updatePartial(prismaClient),
   }
+}
+
+export class EmployeeRepositoryContext extends Context.Tag("Repository/EmployeeRepository")<EmployeeRepositoryContext, Types.EmployeeRepository>() {
+  static Live = Layer.effect(this, Effect.gen(function* () {
+    const prismaClient = yield * PrismaClientContext
+    return initEmployeeRepository(prismaClient)
+  }))
 }
