265c4a1d5e46fc947be9fb36cc0ed0268e5d6431
peeranat-dan <ppeeranat.d@gmail.com>
2025-01-19 14:29:37 +0700

    refactor: implement employee service

--- src/services/employees/index.ts
+++ src/services/employees/index.ts
@@ -1,17 +1,19 @@
-import type { EmployeeRepository } from "../../types/repositories/employee.js"
 import type { EmployeeService } from "../../types/services/employee.js"
+import { Context, Effect, Layer } from "effect"
+import { EmployeeRepositoryContext } from "../../repositories/employees/index.js"
 
-import * as Creates from "./creates.js"
-import * as Finds from "./finds.js"
-import * as Removes from "./removes.js"
-import * as Updates from "./updates.js"
-
-export function initEmployeeService(employeeRepository: EmployeeRepository): EmployeeService {
-  return {
-    create: Creates.create(employeeRepository),
-    findMany: Finds.findMany(employeeRepository),
-    findOneById: Finds.findOneById(employeeRepository),
-    removeById: Removes.removeById(employeeRepository),
-    update: Updates.update(employeeRepository),
-  }
+export class EmployeeServiceContext extends Context.Tag("Service/EmployeeService")<EmployeeServiceContext, EmployeeService>() {
+  static Live = Layer.effect(this, Effect.all({
+    repo: EmployeeRepositoryContext,
+  }).pipe(
+    Effect.andThen(({ repo }) => {
+      return {
+        create: data => repo.create(data).pipe(Effect.withSpan("create.employee.service")),
+        findMany: () => repo.findManyWithRelations().pipe(Effect.withSpan("findMany.employee.service")),
+        findOneById: id => repo.findByIdWithRelations(id).pipe(Effect.withSpan("findById.employee.service")),
+        removeById: id => repo.remove(id).pipe(Effect.withSpan("remove.employee.service")),
+        update: (id, data) => repo.update(id, data).pipe(Effect.withSpan("update.employee.service")),
+      } satisfies EmployeeService
+    }),
+  ))
 }
