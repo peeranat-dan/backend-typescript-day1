ada87aa4c413a1c6d547ac285b5329b4de8e71df
peeranat-dan <ppeeranat.d@gmail.com>
2025-01-19 13:53:36 +0700

    refactor: update employee repository methods to use Effect for error handling and improve flow

--- src/repositories/employees/updates.ts
+++ src/repositories/employees/updates.ts
@@ -1,32 +1,38 @@
 import type { PrismaClient } from "@prisma/client"
 
 import type { EmployeeRepository } from "../../types/repositories/employee.js"
+import { Effect } from "effect"
 import { EmployeeSchema, Helpers } from "../../schema/index.js"
+import * as Errors from "../../types/errors/employees.js"
 
 export function update(prismaClient: PrismaClient): EmployeeRepository["update"] {
-  return async (id, data) => {
-    const result = await prismaClient.employee.update({
+  return (id, data) => Effect.tryPromise({
+    catch: Errors.UpdateEmployeeError.new(),
+    try: () => prismaClient.employee.update({
       data,
       where: {
         deletedAt: null,
         id,
       },
-    })
-
-    return Helpers.fromObjectToSchema(EmployeeSchema.Schema)(result)
-  }
+    }),
+  }).pipe(
+    Effect.andThen(Helpers.fromObjectToSchema(EmployeeSchema.Schema)),
+    Effect.withSpan("update.employee.repository"),
+  )
 }
 
 export function updatePartial(prismaClient: PrismaClient): EmployeeRepository["updatePartial"] {
-  return async (id, data) => {
-    const result = await prismaClient.employee.update({
+  return (id, data) => Effect.tryPromise({
+    catch: Errors.UpdateEmployeeError.new(),
+    try: () => prismaClient.employee.update({
       data,
       where: {
         deletedAt: null,
         id,
       },
-    })
-
-    return Helpers.fromObjectToSchema(EmployeeSchema.Schema)(result)
-  }
+    }),
+  }).pipe(
+    Effect.andThen(Helpers.fromObjectToSchema(EmployeeSchema.Schema)),
+    Effect.withSpan("updatePartial.employee.repository"),
+  )
 }
