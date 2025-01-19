e278dc27f6b0f2304ed6d035fcf62d9844b2203c
peeranat-dan <ppeeranat.d@gmail.com>
2025-01-19 13:45:50 +0700

    refactor: change implementation for creates repository

--- src/repositories/employees/creates.ts
+++ src/repositories/employees/creates.ts
@@ -1,12 +1,18 @@
 import type { PrismaClient } from "@prisma/client"
 import type { EmployeeRepository } from "../../types/repositories/employee.js"
-import { EmployeeSchema, Helpers } from "../../schema/index.js"
+import { Effect } from "effect"
+import * as Helpers from "../../schema/helpers.js"
+import { EmployeeSchema } from "../../schema/index.js"
+import * as Errors from "../../types/errors/employees.js"
 
 export function create(prismaClient: PrismaClient): EmployeeRepository["create"] {
-  return async (data) => {
-    const result = await prismaClient.employee.create({
+  return data => Effect.tryPromise({
+    catch: Errors.CreateEmployeeError.new(),
+    try: () => prismaClient.employee.create({
       data,
-    })
-    return Helpers.fromObjectToSchema(EmployeeSchema.Schema)(result)
-  }
+    }),
+  }).pipe(
+    Effect.andThen(Helpers.fromObjectToSchema(EmployeeSchema.Schema)),
+    Effect.withSpan("create.employee.repository"),
+  )
 }
