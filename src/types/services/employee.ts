5dc77ca3519d08ea38a7e8b443664a014eaf0925
peeranat-dan <ppeeranat.d@gmail.com>
2025-01-19 14:28:20 +0700

    refactor: update service types with effect

--- src/types/services/employee.ts
+++ src/types/services/employee.ts
@@ -1,9 +1,13 @@
+import type { Effect } from "effect"
+import type { NoSuchElementException } from "effect/Cause"
+import type { ParseError } from "effect/ParseResult"
 import type { Branded, EmployeeSchema, EmployeeWithRelationsSchema } from "../../schema/index.js"
+import type * as Errors from "../errors/employees.js"
 
 export type EmployeeService = {
-  create: (data: EmployeeSchema.CreateEmployee) => Promise<EmployeeSchema.Employee>
-  findMany: () => Promise<EmployeeWithRelationsSchema.EmployeeWithRelationsArray>
-  findOneById: (id: Branded.EmployeeId) => Promise<EmployeeWithRelationsSchema.EmployeeWithRelations | null>
-  update: (id: Branded.EmployeeId, data: EmployeeSchema.UpdateEmployee) => Promise<EmployeeSchema.Employee | null>
-  removeById: (id: Branded.EmployeeId) => Promise<EmployeeSchema.Employee | null>
+  create: (data: EmployeeSchema.CreateEmployee) => Effect.Effect<EmployeeSchema.Employee, Errors.CreateEmployeeError | ParseError>
+  findMany: () => Effect.Effect<EmployeeWithRelationsSchema.EmployeeWithRelationsArray, Errors.FindManyEmployeesError>
+  findOneById: (id: Branded.EmployeeId) => Effect.Effect<EmployeeWithRelationsSchema.EmployeeWithRelations, Errors.FindEmployeeByIdError | ParseError | NoSuchElementException>
+  removeById: (id: Branded.EmployeeId) => Effect.Effect<EmployeeSchema.Employee, Errors.RemoveEmployeeError>
+  update: (id: Branded.EmployeeId, data: EmployeeSchema.UpdateEmployee) => Effect.Effect<EmployeeSchema.Employee, Errors.UpdateEmployeeError | ParseError>
 }
